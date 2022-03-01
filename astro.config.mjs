8c694d3d46fe862e6429b3c3b195ffb74e413e43
dosbenjamin <dosbenjamin@icloud.com>
2022-03-01 22:24:29 +0100

    feat: add sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@
 
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
-	// Comment out "renderers: []" to enable Astro's default component support.
-	renderers: [],
-});
+  // Comment out "renderers: []" to enable Astro's default component support.
+  renderers: [],
+  buildOptions: {
+    site: 'https://benjamindossantos.be',
+    sitemap: true
+  }
+})
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,8 @@
 interface ImportMetaEnv {
   readonly STRAPI_URL: string
+  readonly SITE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
