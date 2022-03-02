c74d2f768fc25e53e03847515ffb676bdf910264
dosbenjamin <dosbenjamin@icloud.com>
2022-03-02 22:56:33 +0100

    fix: vite/vercel env variables

--- src/graphql-client.ts
+++ src/graphql-client.ts
@@ -1,4 +1,4 @@
 import { GraphQLClient } from 'graphql-request'
 
 console.log(import.meta.env)
-export const client = new GraphQLClient(import.meta.env.STRAPI_URL)
+export const client = new GraphQLClient(import.meta.env.STRAPI_URL || process.env.STRAPI_URL)
