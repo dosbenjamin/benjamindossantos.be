f4ae06e73cbc18a3e93e6f6455409884392f4d8b
dosbenjamin <dosbenjamin@icloud.com>
2022-03-20 10:07:27 +0100

    feat: add types to process.env

--- src/cloudinary.ts
+++ src/cloudinary.ts
@@ -2,8 +2,8 @@ import { Cloudinary } from '@cloudinary/url-gen'
 
 export const cloudinary = new Cloudinary({
   cloud: {
-    cloudName: import.meta.env.CLOUDINARY_NAME || process.env.CLOUDINARY_NAME,
-    apiKey: import.meta.env.CLOUDINARY_KEY || process.env.CLOUDINARY_KEY,
-    apiSecret: import.meta.env.CLOUDINARY_SECRET || process.env.CLOUDINARY_SECRET
+    cloudName: process.env.CLOUDINARY_NAME || import.meta.env.CLOUDINARY_NAME,
+    apiKey: process.env.CLOUDINARY_KEY || import.meta.env.CLOUDINARY_KEY,
+    apiSecret: process.env.CLOUDINARY_SECRET || import.meta.env.CLOUDINARY_SECRET
   }
 })
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,16 @@
-interface ImportMetaEnv {
+type Env = {
   readonly STRAPI_URL: string
   readonly CLOUDINARY_NAME: string
   readonly CLOUDINARY_KEY: string
   readonly CLOUDINARY_SECRET: string
 }
 
+interface ImportMetaEnv extends Env {}
+
 interface ImportMeta {
   readonly env: ImportMetaEnv
+}
+
+declare namespace NodeJS {
+  interface ProcessEnv extends Env {}
 }
