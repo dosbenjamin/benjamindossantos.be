fb9792edcae06797143235b645ce7e04ce0f8b12
dosbenjamin <dosbenjamin@icloud.com>
2022-03-27 13:50:46 +0200

    fix: wrong strapi image type and meta thumbnail

--- src/types/strapi.ts
+++ src/types/strapi.ts
@@ -3,9 +3,9 @@ import { Cloudinary } from './cloudinary'
 export namespace Strapi {
   export type Attributes <A> = { attributes: A }
   export type Data <T> = { data: Attributes<T> }
-  export type Image = {
+  export type Image = Strapi.Data<{
     url: string
     mime: string
     provider_metadata: Cloudinary.Metadata
-  }
+  }>
 }
