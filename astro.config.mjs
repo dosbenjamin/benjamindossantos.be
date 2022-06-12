cbbd1a0b707cd751bdeef0128bc4c1ec4bfd6eba
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-06-12 17:39:18 +0200

    feat: bump astro version to 1.0 beta

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
+import sitemap from '@astrojs/sitemap'
+
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
   // Comment out "renderers: []" to enable Astro's default component support.
-  integrations: [],
-  buildOptions: {
-    site: 'https://benjamindossantos.be',
-    sitemap: true
-  }
+  site: 'https://benjamindossantos.be',
+  integrations: [sitemap()]
 })
