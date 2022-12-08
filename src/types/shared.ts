bf99e1e44e39b399faa78b35f38df455baa2ec77
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-12-08 16:55:13 +0100

    refactor: replace typescript namespace with import

--- src/types/shared.ts
+++ src/types/shared.ts
@@ -1,28 +1,26 @@
-export namespace Shared {
-  export type Link = {
-    name: string
-    url: string
+export type Link = {
+  name: string
+  url: string
+}
+
+export type Header = {
+  title: string
+  subtitle: string
+  link: Link
+}
+
+export type Global = {
+  networks: Link[]
+}
+
+export namespace SEO {
+  export type Meta = {
+    title?: string
+    description?: string
+    thumbnail?: string
   }
 
-  export type Header = {
-    title: string
-    subtitle: string
-    link: Link
-  }
-
-  export type Global = {
-    networks: Link[]
-  }
-
-  export namespace SEO {
-    export type Meta = {
-      title?: string
-      description?: string
-      thumbnail?: string
-    }
-
-    export type Structured = {
-      json?: {}
-    }
+  export type Structured = {
+    json?: {}
   }
 }
